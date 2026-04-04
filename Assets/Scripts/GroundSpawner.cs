502a47ad4a068fe68dc3f4fc214af186f19e84a1
Gaboo1411 <gabrielgimenezsurdel@gmail.com>
2026-04-03 21:28:28 -0300

    B
    Creacion de los scripts de movimiento, prefab del suelo y generacion de suelo de manera infinita

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    public GameObject groundTile;
+    Vector3 nextSpawnPoint; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+       
+}
